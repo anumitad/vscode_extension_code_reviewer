256725032126e8c2f4a4f90bea5b656a51948199
Anumita Dhobley <114549426+anumitad@users.noreply.github.com>
2025-05-10 14:14:21 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# cr README
+
+This is a code reviewer. 
+
+## Features
+
+It suggests changes to your code, so that you have robust code. 
+
+## Requirements
+
+It runs using gemini, to run it ensure to have a gemini access key. 
+
---
+++ script.py
@@ -0,0 +1,36 @@
+# import system library
+import sys
+from google import genai
+
+# take passed variable values 
+code = str(sys.argv[1])
+language = str(sys.argv[2])
+#print(code)
+
+
+client = genai.Client(api_key="AIzaSyC8yBYewaad5VSzo7LjUFmzuB77Rol7eOg")
+
+
+prompt = f'''<INSTRUCTIONS> \n 
+You are a senior software engineer and expert code reviewer. \n
+Your task is to analyze the following code written in {language} for any bugs, errors, or logical issues. \n
+Be thorough and explain any issues you find. \n
+List each issue you find in the code. \n
+For each issue provide a brief title, the affected line number(s), and an explanation. \n
+If possible, suggest a fix. \n
+If the code is correct, say there are no significant issues. \n  
+The code is as follows: \n 
+<CODE> \n
+{code}'''
+
+
+#print(prompt)
+
+
+#exit()
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents=prompt
+)
+
+print(response.text)
